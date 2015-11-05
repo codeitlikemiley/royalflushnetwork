60b956fc6658c0d87273bcbc6061498292911d71
Uriah Galang <masterpowers001@gmail.com>
2015-11-05 13:45:22 +0000

    added lang auth and titles

---
+++ resources/lang/en/titles.php
@@ -0,0 +1,19 @@
+ <?php
+
+return [
+
+	'app'		=> 'MaxPayout',
+	'app2'		=> 'MaxPayout 2.0',
+
+	'includes'		=> 'Home',
+	'login'		=> 'Login',
+	'logout'	=> 'Logout',
+	'register'	=> 'Register',
+	'resetPword'	=> 'Reset Password',
+	'dashboard' => 'Dashboard',
+	'verifymail' => 'Please Verify Your Email Address',
+	
+	'toggleNav'	=> 'Toggle Navigation',
+	
+
+];
