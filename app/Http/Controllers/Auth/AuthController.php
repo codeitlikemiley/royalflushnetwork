2bf229db9f3225f9d025185bf987969e794ce45a
Uriah Galang <masterpowers001@gmail.com>
2015-11-06 08:30:07 +0000

    Auth Controller Working  Password Resend Login and Reset

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,169 +3,124 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use Auth;
+use Input;
 use Validator;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\EmailRequest;
+use App\Http\Controllers\MailController as Mail;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+    
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     protected $redirectTo = '/profile';
     protected $loginPath = '/login';
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+     public $mail;
+
+    
+    public function __construct(Mail $mail)
     {
        $this->middleware('guest',
             ['except' =>
                 ['getLogout', 'resendEmail', 'activateAccount']]);
+       $this->mail = $mail;
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+    protected function create(Request $request)
     {
-        return Validator::make($data, [
-            'username' => 'alpha_num|required|between:6,30|unique:users',
-            'first_name' => 'required|between:2,30',
-            'last_name' => 'required|between:2,30',
-            'display_name' => 'max:30',
-            'email' => 'required|email|max:60|unique:users',
-            'password' => 'required|confirmed|min:8',
-            'agree' => 'required'
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
-
-
-    public function getRegister()
-    {
-        $message = "Sorry You Cant Register without a Sponsor";
-        return view('nosponsor')->with('message', $message);
-    }
-
-     public function getSponsorLink($link = null)
-    {
-
-        try {
-            $link = Link::where('link', $link)->firstOrFail();
-
-            return view('auth.register')->with(compact('link'));
-        }
-        catch(ModelNotFoundException $e) {
-                $message = "Sorry it Seems that $link is not Yet Registered to Us!";
-                return view('nosponsor')->with('message', $message);
-            }
-    }
-
-    public function postRegister(Request $request)
-    {
-
-        $validator = $this->validator($request->all());
-
+        $data = $request->all();
+        $hash = (str_replace('/', '', \Hash::make(str_random(6))));
+        $data['activation_token'] = $hash;
+        $data['active'] = false;
+        $data['status'] = true;
+        $createUserRequest = new CreateUserRequest();
+        $validator = Validator::make($data, $createUserRequest->rules());
         if ($validator->fails()) {
-            $this->throwValidationException(
-                $request, $validator
-            );
+            return redirect('login')
+                ->withErrors($validator)
+                ->withInput()
+                ->with('signup', 'active');
         }
-        // I should add a Foreign Key for the Link ID to the usertable
-        $activation_code = str_random(60) . $request->input('email');
+        $user = User::create($data);
+        $this->mail->registered($user);
+        return \View::make('auth.success');
 
-        $user = new User;
-        $user->username = $request->input('username');
-        $user->email = $request->input('email');
-        $user->password = bcrypt($request->input('password'));
-        $user->sp_id = $request->input('sp_id');
-        $user->activation_code = $activation_code;
-        $user->save();
+    }
 
-        // Creates the user_id in the profile
-        $profile = new Profile;
-        $profile->first_name = $request->input('first_name');
-        $profile->last_name = $request->input('last_name');
-        $profile->display_name = $request->input('display_name');
-        $user->profile()->save($profile);
-
-        // Creates the user_id in the link
-        $link = new Link;
-        $link->link = $request->input('username');
-        $link->sp_link_id = $request->input('sp_link_id');
-        $link->sp_user_id = $request->input('sp_id');
-        $user->link()->save($link);
-
-        if ($user->save()) {
-
-
-
-            $this->sendEmail($user);
-
-            return view('auth.activateAccount')
-                ->with('email', $request->input('email'));
-
-        } else {
-
-            \Session::flash('message', \Lang::get('notCreated') );
-            return redirect()->back()->withInput();
-
+    /**
+     * check credentials and log in the user.
+     *
+     * @param array $request
+     *
+     * @return view
+     */
+    public function authenticate(Request $request)
+    {
+         $loginRequest = new LoginRequest();
+        $validator = Validator::make($request->all(), $loginRequest->rules(), $loginRequest->messages());
+        if ($validator->fails()) {
+            return response()->json('notregister');
         }
+        $email = $request->email;
+        $password = $request->password;
+        // if (Auth::attempt(['email' => $email, 'password' => $password, 'active' => false, 'status' => false])) {
+        if (Auth::attempt(['email' => $email, 'password' => $password, 'active' => false, 'status' => true])) {
+            return response()->json('notactive');
+                
+                
+        }elseif (Auth::attempt(['email' => $email, 'password' => $password, 'active' => true, 'status' => false])) {
+            Auth::Logout();
+            return response()->json('banned');
+        }
+        elseif (Auth::attempt(['email' => $email, 'password' => $password, 'active' => true, 'status' => true])) {
+            return response()->json('success');
+        }
+        return response()->json('wrongpass');
 
+    }
 
-
-
+    public function login()
+    {
+        return \View::make('auth.login');
     }
 
 
 
-
-    public function sendEmail(User $user)
+    public function activate($email, $activation_code)
     {
 
-        $data = array(
-                'name' => $user->name,
-                'code' => $user->activation_code,
-        );
-        
-        \Mail::queue('emails.activateAccount', $data, function($message) use ($user) {
-            $message->subject( \Lang::get('auth.`') );
-            $message->to($user->email);
-        });
+        $user = User::where('email', $email)
+                ->first();
+        if($user){
+            if($user->activation_code === $activation_code){
+                $user->active = true;
+                $user->status = true;
+                $user->save();
+                $this->mail->activated($user);
+                return \View::make('auth.active');    
+            }
+            
+        }
+        return \View::make('auth.error');
     }
-    
+
+    public function sendActivationLink(EmailRequest $request)
+    {
+        $user = User::where('email', $request->email)->firstOrFail();
+        $this->mail->registered($user);
+        return \View::make('auth.success');
+    }
+
     public function resendEmail()
     {
         $user = \Auth::user();
@@ -176,23 +131,12 @@ class AuthController extends Controller
         } else {
             $user->resent = $user->resent + 1;
             $user->save();
-            $this->sendEmail($user);
-            return view('auth.activateAccount')
-                ->with('email', $user->email);
+            $this->mail->passwordResend($user);
+            \Auth::Logout();
+            return \View::make('auth.success');
         }
+        return \View::make('auth.error');
     }
-    
-    public function activateAccount($code, User $user)
-    {
 
-        if($user->accountIsActive($code)) {
-            \Session::flash('message', \Lang::get('auth.successActivated') );
 
-            return redirect('/dashboard');
-        }
-    
-        \Session::flash('message', \Lang::get('auth.unsuccessful') );
-        return redirect('/dashboard');
-    
-    }
 }
