acca6b2a9a6e2e1c9cc70c374b1c977c24874723
Uriah Galang <masterpowers001@gmail.com>
2015-11-15 11:42:29 +0000

    fix bottom sheet missing div

--- resources/views/materialized.blade.php
+++ resources/views/materialized.blade.php
@@ -1,7 +1,22 @@
 @extends('app')
 
 @section('content')
+ <ul id="dropdown2" class="dropdown-content">
+    <li><a href="#!">1<span class="badge">1</span></a></li>
+    <li><a href="#!">2<span class="new badge">1</span></a></li>
+    <li><a href="#!">3</a></li>
+    <li><a href="#!">4</a></li>
+    <li><a href="#!">5</a></li>
+    <li><a href="#!">6</a></li>
+	<li><a href="#!">7</a></li>
+   	<li><a href="#!">8</a></li>
+   	<li><a href="#!">9</a></li>
+   	<li><a href="#!">10</a></li>
+   	
 
+
+  </ul>
+  <a class="btn dropdown-button" href="#!" data-activates="dropdown2">Dropdown<i class="mdi-navigation-arrow-drop-down right"></i></a>
 
 
  
