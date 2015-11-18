d70f379de67441373c8966c8010c61d2b89798e6
Uriah Galang <masterpowers001@gmail.com>
2015-11-18 15:30:24 +0000

    add description on Model

--- app/Link.php
+++ app/Link.php
@@ -6,29 +6,59 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
+	/**
+     * The database table used by the model.
+     *
+     * @var string
+     */
     protected $table = 'links';
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['link'];
-
+     /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
     protected $hidden = ['id','user_id', 'sp_user_id', 'sp_link_id', 'date_activated', 'created_at', 'updated_at'];
-
+    /**
+     * [$dates That is Casted on Carbon Instance]
+     * @var [Timestamp]
+     */
     protected $dates = ['created_at', 'updated_at', 'date_activated'];
-
+    /**
+     * [findByLink Get Link Object By Link]
+     * @param  [string] $link [registered link]
+     * @return [object]       [linkData]
+     */
     public static function findByLink($link)
     {
         return self::where('link', $link)->firstOrFail();
     }
-
+    /**
+     * [user Eloquent Relationship]
+     * @return [Collection] [Link Belongs To Relationship]
+     */
     public function user()
     {
         return $this->belongsTo('App\User');
     }
-
+    /**
+     * [setLinkAttribute Link Setter Mutator]
+     * @param [string] $value [Set Link to Lowercase on Save]
+     */
     public function setLinkAttribute($value)
     {
         $this->attributes['link'] = strtolower($value);
     }
-
+    /**
+     * [spStatus Get Status of Sponsor Link]
+     * @param  [int] $lid [sp_link_id]
+     * @return [Boolean]      [Return The Status]
+     */
     public function spStatus($lid)
     {
         $sponsor = self::find($lid);
@@ -39,7 +69,11 @@ class Link extends Model
 
         return true;
     }
-
+    /**
+     * [activeSponsor Loops On Leap Frog Until Status Active]
+     * @param  [int] $sp_lid [sp_link_id]
+     * @return [int]         [Returns the First Active Sponsor Link]
+     */
     public function activeSponsor($sp_lid)
     {
         $lid = $sp_lid;
@@ -50,7 +84,11 @@ class Link extends Model
 
         return $lid;
     }
-
+    /**
+     * [leapfrog Sponsor Link if Not Active]
+     * @param  [int] $lid [sp_link_id]
+     * @return [int]      [Return The Next Sponsor if Not Active]
+     */
     public function leapfrog($lid)
     {
         $sponsor = self::find($lid);
--- app/User.php
+++ app/User.php
@@ -42,34 +42,53 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         'status' => 'boolean',
 
     ];
-
+    /**
+     * [findByUsername Find User Using Their Username].
+     *
+     * @param [string] $username [Public Searchable]
+     *
+     * @return [Object] [Userdata]
+     */
     public static function findByUsername($username)
     {
         return self::where('username', $username)->firstOrFail();
     }
-
+    /**
+     * [setPasswordAttribute Password Setter Mutator].
+     *
+     * @param [string] $value [Enforce Bcrypt On Save]
+     */
     public function setPasswordAttribute($value)
     {
         if (!empty($value)) {
             $this->attributes['password'] = \Hash::make($value);
         }
-    } // Mutators Setter before password is save in db
+    }
 
-    
-
+    /**
+     * [setUsernameAttribute Username Setter Mutator].
+     *
+     * @param [string] $value [Make Username in Lowercase on Save]
+     */
     public function setUsernameAttribute($value)
     {
         $this->attributes['username'] = strtolower($value);
-        // always set the string to lowercase
     }
 
-    // LINK RELATION
-
+    /**
+     * [links Eloquent Relationship].
+     *
+     * @return [Collection] [Set User to Have Many Links]
+     */
     public function links()
     {
         return $this->hasMany('App\Link');
     }
-
+    /**
+     * [profile Eloquent Relationship].
+     *
+     * @return [Collection] [Set User to Have One Profile]
+     */
     public function profile()
     {
         return $this->hasOne('App\Profile');
