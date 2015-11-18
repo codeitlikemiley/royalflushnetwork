7f3618a75ba6082d4546f709f6aec139e74a588c
Uriah Galang <masterpowers001@gmail.com>
2015-11-18 04:46:16 +0000

    red button

--- resources/views/layouts/bottomsheet.blade.php
+++ resources/views/layouts/bottomsheet.blade.php
@@ -1,7 +1,7 @@
  	<!-- Modal Structure -->
   <div id="bottom_sheet_home" class="modal bottom-sheet">
     <div class="modal-content">
-    <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat right">CLOSE</a>
+    <a href="#!" class=" modal-action modal-close waves-effect waves-red btn-flat right">CLOSE</a>
      @include('layouts.forms.bottomsheet')
     </div>
     <div class="modal-footer">
