493d537cbd8040ca151c4321bbf69e42eaaaa30a
Uriah Galang <masterpowers001@gmail.com>
2015-11-23 03:56:30 +0000

    Change Model Factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,10 +13,27 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'username' => $faker->userName,
-        'email' => $faker->freeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-        'active' => 1,
+        'username'       => str_replace('.', '_', $faker->unique()->userName),
+        'email'          => $faker->freeEmail,
+        'password'       => 'password',
+        'remember_token' => str_random(64),
+        'active'         => 1,
+        'status'         => 1,
+        'created_at'     => \Carbon\Carbon::now(),
+        'updated_at'     => \Carbon\Carbon::now(),
+    ];
+});
+$factory->define(App\Link::class, function (Faker\Generator $faker) {
+    $users = User::all()->lists('id');
+    $links = Link::all()->lists('id');
+
+    return [
+        'link'           => $faker->userName,
+        'user_id'        => $faker->optional(),
+        'sp_user_id'     => $faker->randomElement($users),
+        'sp_link_id'     => $faker->randomElement($links),
+        'password'       => bcrypt('password'),
+        'remember_token' => str_random(64),
+        'active'         => 1,
     ];
 });
