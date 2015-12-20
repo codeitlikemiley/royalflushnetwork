b31889011f1e42305ba1ad5ab238b8f4468a64a3
Uriah Galang <masterpowers001@gmail.com>
2015-12-20 12:05:42 +0000

    added Cookie link in search bar

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,7 +5,12 @@
        <form action="searchUser" method="POST" id="search_form">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 	        <div class="input-field overflow">
-	          <input id="q" name="q" type="search" required style="color:#e57373;" placeholder="Search Sponsor Username">
+	          <input id="q" name="q" type="search" required style="color:#e57373;" placeholder="Search Sponsor Username"
+              @if(\Cookie::has('sponsor'))
+              {{--*/ $cookie = \Cookie::get('sponsor') /*--}}
+              value="{{ $cookie['link']  }}"
+              @endif
+              >
 	          <label for="search"><i class="material-icons" style="color:#e57373;">search</i></label>
 	          <i class="material-icons" >close</i>
 
