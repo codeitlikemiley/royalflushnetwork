7d7e3c68ab8fd95e4d99f19e76fcfc4095c2a42e
Uriah Galang <masterpowers001@gmail.com>
2015-12-22 22:13:31 +0000

    remove extra clutter in homepage

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -20,6 +20,7 @@
             <li><a class="white-text" href="#!">Terms and Condition</a></li>
             <li><a class="white-text" href="#!">Cookie Usage Policy</a></li>
             <li><a class="white-text" href="#!">Anti-Spam Policy</a></li>
+            <li><a class="white-text" href="#!">Security Policy</a></li>
             <li><a class="white-text" href="#!">Privacy Policy</a></li>
             <li><a class="white-text" href="#!">Termination Policy</a></li>
             <li><a class="white-text" href="#!">Refund Policy</a></li>
--- resources/views/layouts/news.blade.php
+++ resources/views/layouts/news.blade.php
@@ -2,9 +2,9 @@
 <div class="latest_members">
 
 
-<h2>Latest Members</h2>
+<h2>Latest Members<i class="material-icons amber-text">new_releases</i></h2>
 <ul id="users">
-    <li v-for="user in users"><strong class="amber-text">@{{ user.display_name }}</strong> --- <strong>Signed Up!</strong> --- <abbr class="timeago" v-bind:title="user.created_at">@{{ user.created_at }}</abbr></li>
+    <li v-for="user in users"><strong class="amber-text">@{{ user.display_name }}</strong><strong> Joined </strong><abbr class="timeago" v-bind:title="user.created_at">@{{ user.created_at }}</abbr></li>
 
 </ul>
 
